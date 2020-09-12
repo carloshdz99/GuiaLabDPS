bfd2573d80689bdfa974087d641b2d0f253d4f16
carloshdz99 <carloshdzfunes@gmail.com>
2020-09-12 15:23:44 -0600

    validaciones

--- guia04/crudangularphp/src/app/app.component.ts
+++ guia04/crudangularphp/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ArticulosService } from './articulos.service';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,61 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'crudangularphp';
+
+  articulos = null;
+  art = {
+    codigo: 0,
+    descripcion: null,
+    precio: null,
+    proveedor: null,
+    fabricante: null
+  }
+
+  constructor(private articulosServicio: ArticulosService) { }
+
+  ngOnInit() {
+    this.recuperarTodos();
+  }
+
+  recuperarTodos() {
+    this.articulosServicio.recuperarTodos().subscribe(result => this.articulos = result);
+  }
+  
+  alta() {
+    this.articulosServicio.alta(this.art).subscribe(datos => {
+      if (datos['resultado'] == 'OK') {
+        alert(datos['mensaje']);
+        this.recuperarTodos();
+        this.art = { codigo: 0, descripcion: null, precio: null, proveedor: null, fabricante: null };
+      }
+    });
+  }
+
+  baja(codigo) {
+    if (confirm('¿Esta seguro de elimiar el Registro?')) {
+      this.articulosServicio.baja(codigo).subscribe(datos => {
+        if (datos['resultado'] == 'OK') {
+          alert(datos['mensaje']);
+          this.recuperarTodos();
+        }
+      });
+    }
+  }
+
+  modificacion() {
+    this.articulosServicio.modificacion(this.art).subscribe(datos => {
+      if (datos['resultado'] == 'OK') {
+        alert(datos['mensaje']);
+        this.recuperarTodos();
+        this.art = { codigo: 0, descripcion: null, precio: null, proveedor: null, fabricante: null };
+      }
+    });
+  }
+
+  seleccionar(codigo) {
+    this.articulosServicio.seleccionar(codigo).subscribe(result => this.art = result[0]);
+  }
+  hayRegistros() {
+    return true;
+  }
 }
