12d58ed4df1c3f61ffc682d1bab65eb12b2e897c
carloshdz99 <carloshdzfunes@gmail.com>
2020-09-19 22:41:27 -0600

    discusion de resultados

--- guia05/alumnosFirebase/src/app/app.module.ts
+++ guia05/alumnosFirebase/src/app/app.module.ts
@@ -4,15 +4,43 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FormsModule } from '@angular/forms';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+
+//importando firebase
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { environment } from '../environments/environment';
+
+//importando componentes
+import { AlumnosComponent } from './components/alumnos/alumnos.component';
+import { AlumnosListaComponent } from './components/alumnos/alumnos-lista/alumnos-lista.component';
+import { Alumno1Component } from './components/alumnos/alumno1/alumno1.component';
+
+//importando servicios
+import { AlumnoService } from './services/alumno.service';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlumnosComponent,
+    AlumnosListaComponent,
+    Alumno1Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    AlumnoService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
